2bbe426b3374c19d1b4a2c1776bdb831963987b9
Math <mathilde.hetru@gmail.com>
2021-04-09 11:46:22 +0200

    start fizzBuzz

---
+++ Fizzbuzz.js
@@ -0,0 +1,27 @@
+"1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz"
+
+tous les chiffres entiers au multiple de 3 = fizz
+tous les chiffres entiers au multiple de 5 = buzz
+tous les chiffres entiers au multiple de 3 & 5 = fizzbuzz
+
+
+nombres entiers de 1 à 100 
+    SI le nombre est divisible par 3 
+    ALORS fizz
+
+    SI le nombre est divisible par 5
+    ALORS buzz
+
+    SI le nombre est divisible par 3 ET 5 
+    ALORS fizzbuzz
+
+var nombre = 100;
+var multipleDe3 = 3;
+var multipleDe5 = 5;
+
+function deUnACent(nombre) {
+    for (var i = 1; i < nombre; i++)
+        if ()
+}
+
+
